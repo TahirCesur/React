4668391a0a5253163691f9575617022cbf02abb4
Tahir Cesur <delenkar2825@gmail.com>
2021-11-28 15:41:19 +0300

    F01

--- 01-my-app/src/App.js
+++ 01-my-app/src/App.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from "react";
-import {Container, Row, Col} from "react-bootstrap";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Menu from "./components/shared/Menu";
 import Header from "./components/shared/Header";
@@ -28,106 +28,106 @@ import UseEffectPage from "./pages/UseEffectPage";
 import UserCardsPage from "./pages/UserCardsPage";
 import ToDoAppPage from "./pages/ToDoAppPage";
 import ExchangePage from "./pages/ExchangePage";
-import {StoreContext} from "./store";
+import { StoreContext } from "./store";
 import axios from "axios";
 
 function App() {
-    const [kurlar, setKurlar] = useState({});
+  const [kurlar, setKurlar] = useState({});
 
-    useEffect(() => {
-        axios("https://api.frankfurter.app/latest?from=try").then((resp) => {
-            console.log(resp.data);
-            setKurlar(resp.data.rates);
-        });
-    }, []);
+  useEffect(() => {
+    axios("https://api.frankfurter.app/latest?from=try").then((resp) => {
+      console.log(resp.data);
+      setKurlar(resp.data.rates);
+    });
+  }, []);
 
-    return (
-        <StoreContext.Provider value={{kurlar}}>
-            <Router>
-                <Header/>
-                <Container className="mb-5">
-                    <Row>
-                        <Col md={3}>
-                            <Menu/>
-                        </Col>
-                        <Col md={9} className="p-4">
-                            <Switch>
-                                <Route path="/exchange">
-                                    <ExchangePage/>
-                                </Route>
-                                <Route path="/todoapp">
-                                    <ToDoAppPage/>
-                                </Route>
-                                <Route path="/usercards">
-                                    <UserCardsPage/>
-                                </Route>
-                                <Route path="/useeffect">
-                                    <UseEffectPage/>
-                                </Route>
-                                <Route path="/state">
-                                    <StatePage/>
-                                </Route>
-                                <Route path="/shop">
-                                    <ShopPage/>
-                                </Route>
-                                <Route path="/random-images">
-                                    <RandomImagesPage/>
-                                </Route>
-                                <Route path="/progress">
-                                    <ProgressPage/>
-                                </Route>
-                                <Route path="/profile">
-                                    <ProfilePage/>
-                                </Route>
-                                <Route path="/posts">
-                                    <PostsPage/>
-                                </Route>
-                                <Route path="/form4">
-                                    <Form4Page/>
-                                </Route>
-                                <Route path="/form3">
-                                    <Form3Page/>
-                                </Route>
-                                <Route path="/form2">
-                                    <Form2Page/>
-                                </Route>
-                                <Route path="/form1">
-                                    <Form1Page/>
-                                </Route>
-                                <Route path="/countries">
-                                    <CountriesPage/>
-                                </Route>
-                                <Route path="/counter">
-                                    <CounterPage/>
-                                </Route>
-                                <Route path="/comp">
-                                    <CompPage/>
-                                </Route>
-                                <Route path="/clock1">
-                                    <Clock1Page/>
-                                </Route>
-                                <Route path="/clock2">
-                                    <Clock2Page/>
-                                </Route>
-                                <Route path="/clock3">
-                                    <Clock3Page/>
-                                </Route>
-                                <Route path="/bs-classic">
-                                    <BoostrapClassicPage/>
-                                </Route>
-                                <Route path="/birthday">
-                                    <BirthdayPage/>
-                                </Route>
-                                <Route path="/">
-                                    <HomePage/>
-                                </Route>
-                            </Switch>
-                        </Col>
-                    </Row>
-                </Container>
-            </Router>
-        </StoreContext.Provider>
-    );
+  return (
+    <StoreContext.Provider value={{ kurlar }}>
+      <Router>
+        <Header />
+        <Container className="mb-5">
+          <Row>
+            <Col md={3}>
+              <Menu />
+            </Col>
+            <Col md={9} className="p-4">
+              <Switch>
+                <Route path="/exchange">
+                  <ExchangePage />
+                </Route>
+                <Route path="/todoapp">
+                  <ToDoAppPage />
+                </Route>
+                <Route path="/usercards">
+                  <UserCardsPage />
+                </Route>
+                <Route path="/useeffect">
+                  <UseEffectPage />
+                </Route>
+                <Route path="/state">
+                  <StatePage />
+                </Route>
+                <Route path="/shop">
+                  <ShopPage />
+                </Route>
+                <Route path="/random-images">
+                  <RandomImagesPage />
+                </Route>
+                <Route path="/progress">
+                  <ProgressPage />
+                </Route>
+                <Route path="/profile">
+                  <ProfilePage />
+                </Route>
+                <Route path="/posts">
+                  <PostsPage />
+                </Route>
+                <Route path="/form4">
+                  <Form4Page />
+                </Route>
+                <Route path="/form3">
+                  <Form3Page />
+                </Route>
+                <Route path="/form2">
+                  <Form2Page />
+                </Route>
+                <Route path="/form1">
+                  <Form1Page />
+                </Route>
+                <Route path="/countries">
+                  <CountriesPage />
+                </Route>
+                <Route path="/counter">
+                  <CounterPage />
+                </Route>
+                <Route path="/comp">
+                  <CompPage />
+                </Route>
+                <Route path="/clock1">
+                  <Clock1Page />
+                </Route>
+                <Route path="/clock2">
+                  <Clock2Page />
+                </Route>
+                <Route path="/clock3">
+                  <Clock3Page />
+                </Route>
+                <Route path="/bs-classic">
+                  <BoostrapClassicPage />
+                </Route>
+                <Route path="/birthday">
+                  <BirthdayPage />
+                </Route>
+                <Route path="/">
+                  <HomePage />
+                </Route>
+              </Switch>
+            </Col>
+          </Row>
+        </Container>
+      </Router>
+    </StoreContext.Provider>
+  );
 }
 
 export default App;
--- 01-my-app/src/components/clock1/Clock1.js
+++ 01-my-app/src/components/clock1/Clock1.js
@@ -1,30 +1,34 @@
 import "./clock1.css";
-import moment from "moment";
+import moment from "moment"; // moment kütüphanesini import ettik
 
 const Clock1 = () => {
-    const dateTime = moment();
-    const timeStr = dateTime.format("HH:mm");
-    const dateStr = dateTime.format("LL");
-    const dayStr = dateTime.format("dddd");
-    const hour = dateTime.format("HH");
+  // import edilen moment kütüphanesini burada tanımlamış olduk...
+  const dateTime = moment(); /* O an ki saat ve tarihi vermeye yarar... */
+  const timeStr = dateTime.format("HH:mm"); /* saat ve dakika */
+  const dateStr = dateTime.format("LL"); // November 28, 2021 bu şekli verir...
+  const dayStr = dateTime.format("dddd"); // Sunday --> gibi gün verir
+  const hour = dateTime.format("HH"); // Saati alarak Evening Afternon Morning vermesini sağlayacağız...
 
-    let message = "";
-    if (hour >= 6 && hour < 11) message = "Morning";
-    else if (hour >= 11 && hour < 17) message = "Afternoon";
-    else if (hour >= 17 && hour < 21) message = "Evening";
-    else message = "Night";
+  // 9. satırı şartla buraya sağlamış oluyoruz.
+  let message = ""; // let ile tanımlama sebebimiz let değişkendir const ise sabittir.
+  if (hour >= 6 && hour < 11) message = "Morning";
+  else if (hour >= 11 && hour < 17) message = "Afternoon";
+  else if (hour >= 17 && hour < 21) message = "Evening";
+  else message = "Night";
 
-    return (
-        <div className="clock-container">
-            <div className="clock">{timeStr}</div>
-            <div>
-                <div className="date">{dateStr}</div>
-                <div className="day">
-                    {dayStr} {message}
-                </div>
-            </div>
+  return (
+    <div className="clock-container">
+      <div className="clock">{timeStr}</div>
+      <div>
+        <div className="date">{dateStr}</div>
+        <div className="day">
+          {dayStr} {message}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Clock1;
+
+/*  */

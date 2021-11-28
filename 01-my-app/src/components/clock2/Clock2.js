15069bcad6c7a1e4a78b356ffef59f46903764ef
Tahir Cesur <delenkar2825@gmail.com>
2021-11-28 17:51:53 +0300

    F02

--- 01-my-app/src/components/clock2/Clock2.js
+++ 01-my-app/src/components/clock2/Clock2.js
@@ -2,32 +2,36 @@ import "./clock2.css";
 import moment from "moment";
 
 const Clock2 = (props) => {
-    const dateTime = moment();
-    const timeStr = dateTime.format("HH:mm");
-    const dateStr = dateTime.format("LL");
-    const dayStr = dateTime.format("dddd");
-    const hour = dateTime.format("HH");
+  const dateTime = moment();
+  const timeStr = dateTime.format("HH:mm");
+  const dateStr = dateTime.format("LL");
+  const dayStr = dateTime.format("dddd");
+  const hour = dateTime.format("HH");
 
-    let message = "";
-    if (hour >= 6 && hour < 11) message = "Morning";
-    else if (hour >= 11 && hour < 17) message = "Afternoon";
-    else if (hour >= 17 && hour < 21) message = "Evening";
-    else message = "Night";
+  let message = "";
+  if (hour >= 6 && hour < 11) message = "Morning";
+  else if (hour >= 11 && hour < 17) message = "Afternoon";
+  else if (hour >= 17 && hour < 21) message = "Evening";
+  else message = "Night";
 
-    return (
-        <div
-            className="clock-container"
-            style={{color: props.yaziRengi, backgroundColor: props.zeminRengi}}
-        >
-            <div className="clock">{timeStr}</div>
-            <div>
-                <div className="date">{dateStr}</div>
-                <div className="day">
-                    {dayStr} {message}
-                </div>
-            </div>
+  return (
+    <div
+      className="clock-container"
+      style={{ color: "yellow", backgroundColor: "blue" }}
+    >
+      <div className="clock">{timeStr}</div>
+      <div>
+        <div className="date">{dateStr}</div>
+        <div className="day">
+          {dayStr} {message}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Clock2;
+
+//* 20. satır daki style etiketi "clock2.css" dosyasını ezdi...*/
+//? style={{color: props.yaziRengi, backgroundColor: props.zeminRengi}} böyle idi 20. satır..
+//! fakat props oluşturduğumuz dosya proje haline geldiği için değişiklik görünmedi...

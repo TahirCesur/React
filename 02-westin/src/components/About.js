bf594442e81f726ed5ef97f2e18faccf03276d25
Tahir Cesur <delenkar2825@gmail.com>
2021-11-29 00:12:40 +0300

    F02

--- 02-westin/src/components/About.js
+++ 02-westin/src/components/About.js
@@ -1,69 +1,72 @@
 import React from "react";
 
+// Hazır aldığımız formattan sadece class ları className yapmış olduk...
+// 40,45,50,55,60. satırlardaki img tagını / işareti ile kapatmamız gerekiyordu kapatmış olduk...
+// <a href="javascript:void(0)"></a> te "javascript:void(0)"" olan yeri "#"" işareti ile değiştirdik...
 const About = () => {
-    return (
-        <section id="about" className=" about-02 py-6">
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-6">
-                        <div className="about-image">
-                            <img src="assets/img/about-05.jpg.jfif" alt="/"/>
-                        </div>
-                    </div>
-                    <div className="col-lg-6 mt-4 mt-lg-0">
-                        <div className="row">
-                            <div className="col-lg-12">
-                                <div className="title-box text-left">
-                                    <h6>about me</h6>
-                                    <h3>
-                                        My mission is design develop the best Web sites around....
-                                    </h3>
-                                </div>
-                            </div>
-                        </div>
-                        <p className="about-text">
-                            Hello! I'm John Doe. Web designer from USA, California, San
-                            Francisco. I have rich experience in web site design and building,
-                            also I am good at wordpress. I love to talk with you about our
-                            unique.I help you build brand for your business
-                        </p>
-                        <a href="#contact" className="pill-button-01 to-contact">
-                            Hire Me
-                        </a>
-                        <div className="social-icon">
-                            <ul className="list-inline py-2 mb-0">
-                                <li className="list-inline-item">
-                                    <a href="#">
-                                        <img src="assets/img/facebook.svg" alt="/"/>
-                                    </a>
-                                </li>
-                                <li className="list-inline-item">
-                                    <a href="#">
-                                        <img src="assets/img/twitter.svg" alt="/"/>
-                                    </a>
-                                </li>
-                                <li className="list-inline-item">
-                                    <a href="#">
-                                        <img src="assets/img/github.svg" alt="/"/>
-                                    </a>
-                                </li>
-                                <li className="list-inline-item">
-                                    <a href="#">
-                                        <img src="assets/img/linkedin.svg" alt="/"/>
-                                    </a>
-                                </li>
-                                <li className="list-inline-item">
-                                    <a href="#">
-                                        <img src="assets/img/telegram.svg" alt="/"/>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <section id="about" className=" about-02 py-6">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="about-image">
+              <img src="assets/img/about-05.jpg.jfif" alt="/" />
             </div>
-        </section>
-    );
+          </div>
+          <div className="col-lg-6 mt-4 mt-lg-0">
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="title-box text-left">
+                  <h6>about me</h6>
+                  <h3>
+                    My mission is design develop the best Web sites around....
+                  </h3>
+                </div>
+              </div>
+            </div>
+            <p className="about-text">
+              Hello! I'm John Doe. Web designer from USA, California, San
+              Francisco. I have rich experience in web site design and building,
+              also I am good at wordpress. I love to talk with you about our
+              unique.I help you build brand for your business
+            </p>
+            <a href="#contact" className="pill-button-01 to-contact">
+              Hire Me
+            </a>
+            <div className="social-icon">
+              <ul className="list-inline py-2 mb-0">
+                <li className="list-inline-item">
+                  <a href="#">
+                    <img src="assets/img/facebook.svg" alt="/" />
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a href="#">
+                    <img src="assets/img/twitter.svg" alt="/" />
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a href="#">
+                    <img src="assets/img/github.svg" alt="/" />
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a href="#">
+                    <img src="assets/img/linkedin.svg" alt="/" />
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a href="#">
+                    <img src="assets/img/telegram.svg" alt="/" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;

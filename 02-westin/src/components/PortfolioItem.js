3a261cf39e5e8abf095acd5b88077f79bbe233f2
Tahir Cesur <delenkar2825@gmail.com>
2021-11-29 12:45:37 +0300

    F02

--- 02-westin/src/components/PortfolioItem.js
+++ 02-westin/src/components/PortfolioItem.js
@@ -1,26 +1,29 @@
 import React from "react";
 
+// Hazır aldığımız formattan sadece class ları className yapmış olduk...
+// Değişiklik yaptığımız yerler props. olan yerler...
+
 const PortfolioItem = (props) => {
-    return (
-        <div className="portfolio-box">
-            <div className="portfolio-img">
-                <img src={props.resim} alt="/"/>
-            </div>
-            <div className="portfolio-overlay">
-                <div className="portfolio-overlay-content">
-                    <h5 className="portfolio-category">{props.baslik}</h5>
-                    <div className="portfolio-icon">
-                        <a href="#">
-                            <i className="fas fa-link"></i>
-                        </a>
-                        <a href={props.resim} className="js-zoom-gallery">
-                            <i className="fas fa-search-plus"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="portfolio-box">
+      <div className="portfolio-img">
+        <img src={props.resim} alt="/" />
+      </div>
+      <div className="portfolio-overlay">
+        <div className="portfolio-overlay-content">
+          <h5 className="portfolio-category">{props.baslik}</h5>
+          <div className="portfolio-icon">
+            <a href="#">
+              <i className="fas fa-link"></i>
+            </a>
+            <a href={props.resim} className="js-zoom-gallery">
+              <i className="fas fa-search-plus"></i>
+            </a>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default PortfolioItem;

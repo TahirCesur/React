909f979c509e6211c3562e72729ac4d64106e9db
Tahir Cesur <delenkar2825@gmail.com>
2021-11-29 14:29:14 +0300

    F02

--- 02-westin/src/components/TestimonalItem.js
+++ 02-westin/src/components/TestimonalItem.js
@@ -1,19 +1,22 @@
 import React from "react";
 
+// Hazır aldığımız formattan sadece class ları className yapmış olduk...
+// Değişiklik yaptığımız yerler props. olan yerler...
+
 const TestimonalItem = (props) => {
-    return (
-        <div className="testimonial-item">
-            <div className="testimonial-image">
-                <img src={props.resim} alt="/"/>
-            </div>
-            <div className="testimonial-content">
-                <p className="testimonial-comment">"{props.mesaj}"</p>
-                <h6 className="testimonial-name">
-                    {props.isim}, <span className="testimonial-job">{props.unvan}</span>
-                </h6>
-            </div>
-        </div>
-    );
+  return (
+    <div className="testimonial-item">
+      <div className="testimonial-image">
+        <img src={props.resim} alt="/" />
+      </div>
+      <div className="testimonial-content">
+        <p className="testimonial-comment">"{props.mesaj}"</p>
+        <h6 className="testimonial-name">
+          {props.isim}, <span className="testimonial-job">{props.unvan}</span>
+        </h6>
+      </div>
+    </div>
+  );
 };
 
 export default TestimonalItem;

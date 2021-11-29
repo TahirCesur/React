f7e0754ff9c22a1e29fe78ac3872fad41f0810e2
Tahir Cesur <delenkar2825@gmail.com>
2021-11-29 12:36:02 +0300

    F02

--- 02-westin/src/components/SectionTitle.js
+++ 02-westin/src/components/SectionTitle.js
@@ -1,13 +1,15 @@
 import React from "react";
 
+// Hazır aldığımız formattan sadece class ları className yapmış olduk...
+// h6 h3 ve span a props. gibi değişiklikler yaptık..
 const SectionTitle = (props) => {
-    return (
-        <div className="title-box">
-            <h6>{props.aciklama}</h6>
-            <h3>{props.baslik}</h3>
-            <span>{props.baslik}</span>
-        </div>
-    );
+  return (
+    <div className="title-box">
+      <h6>{props.aciklama}</h6>
+      <h3>{props.baslik}</h3>
+      <span>{props.baslik}</span>
+    </div>
+  );
 };
 
 export default SectionTitle;
--- 02-westin/src/components/Service.js
+++ 02-westin/src/components/Service.js
@@ -1,15 +1,18 @@
 import React from "react";
 
+// Hazır aldığımız formattan sadece class ları className yapmış olduk...
+// Değişiklik yaptığımız yerler props. olan yerler...
+
 const Service = (props) => {
-    return (
-        <div className="services-box">
-            <img src={props.resim} alt="/"/>
-            <div className="services-content">
-                <h5 className="services-head">{props.baslik}</h5>
-                <p className="services-description mb-0">{props.aciklama}</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="services-box">
+      <img src={props.resim} alt="/" />
+      <div className="services-content">
+        <h5 className="services-head">{props.baslik}</h5>
+        <p className="services-description mb-0">{props.aciklama}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Service;

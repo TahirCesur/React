e4858d8568d5a974b30475c4a236b4676f5c4548
Tahir Cesur <delenkar2825@gmail.com>
2021-11-30 07:55:41 +0300

    F03

--- 01-my-app/src/components/counter/Counter.js
+++ 01-my-app/src/components/counter/Counter.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import { ButtonGroup, Button } from "react-bootstrap";
 
+// 1. olarak useState import edilmeli...
+
+// 2. olarak useState tanımlanmalı
+// sayac değişkeni, değeri saklar, setSayac ise değeri değiştiren method tur.
+
 const Counter = () => {
   const [sayac, setSayac] = useState(0);
 
@@ -37,3 +42,6 @@ const Counter = () => {
 };
 
 export default Counter;
+
+// 30. satırda disabled yaptık ki gri olan button kısmına basılamasın....
+// {sayac} state ini de buraya ekleyip bağlamış olduk...
